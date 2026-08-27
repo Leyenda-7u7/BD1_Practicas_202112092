723d0bcc231ddae7cc645068b4057464222b5e6c
Brandon Eduardo Pablo Garcia <110555561+Leyenda-7u7@users.noreply.github.com>
2026-08-26 23:04:20 -0600

    Consultas

---
+++ PRACTICA 2/Consulta2.sql
@@ -0,0 +1,9 @@
+
+EMPRESA                                                                                                                                                TOTAL_PLAZAS
+------------------------------------------------------------------------------------------------------------------------------------------------------ ------------
+FinTech Guatemala                                                                                                                                                 2
+TechNova Solutions                                                                                                                                                2
+Agropecuaria El Sol                                                                                                                                               1
+Industrias La Aurora                                                                                                                                              1
+Servicios Contables GT                                                                                                                                            1
+
